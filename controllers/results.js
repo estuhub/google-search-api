d344389508100cc11562676d64e7770b38f3e3c3
estuhub <107235863+estuhub@users.noreply.github.com>
2022-09-14 13:34:03 +0700

    add obj to render data

--- controllers/results.js
+++ controllers/results.js
@@ -2,9 +2,61 @@
 const express = require('express')
 const router = express.Router()
 
+//Data
+let results = [
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  },
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  },
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  }
+]
+
+//Views
 // Create POST controller
 router.get('/results', (req, res) => {
-  res.render('results')
+  res.render('results', {
+    resultsKey: results
+  })
 })
 
 // Export module
