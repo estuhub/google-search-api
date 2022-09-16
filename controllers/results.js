89ecf91629e51c84a291501932a80707c42adca7
estuhub <107235863+estuhub@users.noreply.github.com>
2022-09-16 15:33:08 +0700

    create DB query

--- controllers/results.js
+++ controllers/results.js
@@ -1,79 +1,27 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
-
-//Data
-let results = [
-  {
-    title: 'videos',
-    description: 'The best JavaScript tutorials in the galaxy!',
-    url: 'https://www.w3schools.com',
-    links: [
-      {
-        title: 'JS for Beginners',
-        url: 'https://www.w3schools.com/js'
-      },
-      {
-        title: 'JS for the Web',
-        url: 'https://www.w3schools.com/js'
-      }
-    ]
-  },
-  {
-    title: 'tutorials',
-    description: 'The best JavaScript tutorials in the galaxy!',
-    url: 'https://www.w3schools.com',
-    links: [
-      {
-        title: 'JS for Beginners',
-        url: 'https://www.w3schools.com/js'
-      },
-      {
-        title: 'JS for the Web',
-        url: 'https://www.w3schools.com/js'
-      }
-    ]
-  },
-  {
-    title: 'pictures',
-    description: 'The best JavaScript tutorials in the galaxy!',
-    url: 'https://www.w3schools.com',
-    links: [
-      {
-        title: 'JS for Beginners',
-        url: 'https://www.w3schools.com/js'
-      },
-      {
-        title: 'JS for the Web',
-        url: 'https://www.w3schools.com/js'
-      }
-    ]
-  }
-]
+const Results = require('../models/results')
 
 //Views
-// Create GET controller
-// router.get('/results', (req, res) => {
-//   res.render('results', {
-//     resultsKey: results
-//   })
-// })
-
 const filterResults = (arr, search) => {
   return arr.filter(result => result.title.includes(search))
 }
 
 // Create POST controller
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  // Read from DB with .find({}) waiting with async/await
+  let resultsKey = await Results.find({})
   // log result on terminal
-  // console.log(req.body)
+  // console.log(resultsKey)
 
   // this is the input <input type="text" name="search" /> of the user from search.hbs
   let searchValue = req.body.search
 
   // this is what you render
   res.render('results', {
-    resultsKey: filterResults(results, searchValue)
+    // results
+    resultsKey: filterResults(resultsKey, searchValue)
   })
 })
 
--- models/results.js
+++ models/results.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose')
 
 // Create the results moodel
+let Results = mongoose.model('results', {
+  title: {
+    type: String,
+    require: true
+  },
+  description: {
+    type: String,
+    require: true
+  },
+  url: {
+    type: String,
+    require: true
+  },
+  links: [{ title: String, url: String }]
+})
+
+// Export module
+module.exports = Results
